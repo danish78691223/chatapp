b7eb01b2c482237f91c02d9c6bb0429a11da881f
Danish Khan <askdnk2523@gmail.com>
2025-11-25 22:30:19 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+// Connect to backend Render URL
+const socket = io(process.env.REACT_APP_API_URL, {
   transports: ["websocket"],
   reconnection: true,
   reconnectionAttempts: 10,
