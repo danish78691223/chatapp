2df53a0961fa8da0dcc751d407dac3b0c3b1077d
Danish Khan <askdnk2523@gmail.com>
2026-09-24 22:42:59 +0530

    Fix Render startup by removing invalid dns.setServer call

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ import http from "http";
 import path from "path";
 import fs from "fs";
 import { Server } from "socket.io";
-import dns from "dns";
 
 // ROUTES
 import authRoutes from "./src/routes/authRoutes.js";
@@ -24,7 +23,6 @@ import downloadRoutes from "./src/routes/downloadRoutes.js";
 
 dotenv.config();
 
-dns.setServer(["1.1.1.1", "8.8.8.8"]);
 
 const app = express();
 const server = http.createServer(app);
