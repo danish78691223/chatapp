a66044a0dee6c5fb86aea80912a27114a1ec0b61
Danish Khan <askdnk2523@gmail.com>
2026-09-24 20:01:16 +0530

    feat: expose message reactions and read receipt routes

--- server/src/routes/messageRoutes.js
+++ server/src/routes/messageRoutes.js
@@ -4,13 +4,17 @@ import {
   sendMessage, 
   getMessages, 
   getGroupMessages, 
-  sendGroupMessage 
+  sendGroupMessage,
+  markMessageRead,
+  reactToMessage 
 } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 router.get("/group/:groupId", protect, getGroupMessages);
 router.post("/group/:groupId", protect, sendGroupMessage);
+router.patch("/:messageId/read", protect, markMessageRead);
+router.post("/:messageId/reaction", protect, reactToMessage);
 
 // Private chat
 router.post("/", protect, sendMessage);
